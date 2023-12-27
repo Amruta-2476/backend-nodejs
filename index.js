791b020f1f72159cb75c6696101905b693eb153b
Amruta Mahesh Pawar <amr.paw.rt22@dypatil.edu>
2023-12-27 11:33:02 +0530

    dbConfig

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-var http = require('http');
-var data = require('./test')
-var url = require('url');
+const dotenv = require('dotenv')
+dotenv.config()
+const express = require('express');
+const app = express();
+const db = require('./db_config')
+const PORT = process.env.PORT || 5000;
 
-http.createServer(function (req, res) {
-    // res.writeHead(200,{'Content-Type':'application/son'})
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.write('<h1>RAIT</h1>' + data.testModule())
-    res.write("<br>")
-    // res.write(req.url)
-    var query = url.parse(req.url, true).query;
-    // res.write(query)
-    console.log(query)
-    res.end('<h3>welcome to rait...</h3>')
-}).listen(8787);
+app.use(express.json());   //middleware..
+app.use(express.urlencoded({ extended: true }))
+db.dbConfig();
 
-//// hp-v22ow-64-gb-pen-drive/p/itmffqq2ehgega7a?pid=ACCG4SERD4X9STAF&lid=LSTACCG4SERD4X9STAFTPORHY&marketplace=FLIPKART&store=6bo%2Fjdy&srno=b_1_1&otracker=browse&fm=organic&iid=en_7N9rEIYW-dA14KbAA_omKGWPIe5FL-rho_dwxO2IUzYHp0XhZlSYw0h4sMCO2aI3C1qOm0NaML1ZsnRKDimLzA%3D%3D&ppt=browse&ppn=browse&ssid=ekgl7y720w0000001703571379275
+// const connectionParams = {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true
+// }
+
+app.get('/', (req, res) => {
+    // res.send("this is a test data...")
+    res.json({
+        status: 200,
+        message: "your server is up and running",
+        uri: uri
+    })  //http://localhost:8787/
+})
+app.get('/home', (req, res) => {
+    res.json("this is home router")
+})  //http://localhost:8787/home
+
+app.listen(PORT, () => {
+    console.log(`listening on port number -> ${PORT}`);
+})
