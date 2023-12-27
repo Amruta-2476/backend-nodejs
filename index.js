cc71d9f50a634fbac20839858f3c8b84bf7f24bd
Amruta Mahesh Pawar <amr.paw.rt22@dypatil.edu>
2023-12-27 12:43:05 +0530

    router

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 const db = require('./db_config')
 const PORT = process.env.PORT || 5000;
+// controller import 
+const userRouter = require('./controller/user.controller')
 
 app.use(express.json());   //middleware..
 app.use(express.urlencoded({ extended: true }))
@@ -20,13 +22,43 @@ app.get('/', (req, res) => {
     res.json({
         status: 200,
         message: "your server is up and running",
-        uri: uri
+        // uri: uri
     })  //http://localhost:8787/
 })
-app.get('/home', (req, res) => {
-    res.json("this is home router")
-})  //http://localhost:8787/home
+
+
+
+app.use('/api/v1/users', userRouter) //endpoint
+// app.use('/api/v1/orders')
+// app.use('/api/v1/employees')
+// app.use('/api/v1/blogs')
+// app.use('/api/v1/post')
+
+// app.get('/home', (req, res) => {
+//     res.json("this is home router")
+// })  //http://localhost:8787/home
 
 app.listen(PORT, () => {
     console.log(`listening on port number -> ${PORT}`);
 })
+
+// http://localhost:8787/api/v1/users/createuser
+// http://localhost:8787/api/v1/users/
+// http://localhost:8787/api/v1/users/update
+// http://localhost:8787/api/v1/users/delete
+// http://localhost:8787/api/v1/users/findone
+// http://localhost:8787/api/v1/users/deleteall
+// http://localhost:8787/api/v1/users/findactiveuser
+
+
+
+// users
+
+// create user
+// findall
+// update
+// delete
+// find one
+// delete all
+// find active users
+
